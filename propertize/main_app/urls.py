4aeaea68f2d7a54614184324057fc24c753d8e7f
calebj4 <calebjohnson@Calebs-Laptop.local>
2023-05-02 15:49:59 -0400

    adding login,signup, about, and detail page

--- propertize/main_app/urls.py
+++ propertize/main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.home, name="home"),
     path("about/", views.about, name="about"),
     path("properties/", views.properties_index, name="index"),
-    path("properties/create/", views.PropertyCreate.as_view(), name='properties_create')
+    path("properties/create/", views.PropertyCreate.as_view(), name='properties_create'),
 ]
