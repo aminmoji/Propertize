eaec11cbcb6dfe42184ea41622ab3f8780698965
aminmoj <aminmoj@gmail.com>
2023-05-02 16:54:19 -0400

    signup correction

--- propertize/main_app/views.py
+++ propertize/main_app/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from .forms import PropertyForm, ShowingForm
+from .forms import PropertyForm, ShowingForm, CustomUserCreationForm
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -27,20 +28,26 @@ def property_detail(request, property_id):
     })
 
 
-def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-       form = UserCreationForm(request.POST)
-       if form.is_valid():
-           user = form.save()
-           login(request, user)
-           return redirect('index')
-       else:
-           error_message = 'Invalid Sign Up Attempt, Please Try Again.'
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return redirect(request, 'registration/signup.html', context)
+
+# def signup(request):
+#     error_message = ''
+#     if request.method == 'POST':
+#        form = UserCreationForm(request.POST)
+#        if form.is_valid():
+#            user = form.save()
+#            login(request, user)
+#            return redirect('index')
+#        else:
+#            error_message = 'Invalid Sign Up Attempt, Please Try Again.'
+
+#     form = UserCreationForm()
+#     context = {'form': form, 'error_message': error_message}
+#     return redirect(request, 'registration/signup.html', context)
 
 
 @login_required
